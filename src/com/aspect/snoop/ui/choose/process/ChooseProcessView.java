149cf19268a34f03c0b00825343e0f98e4fb143c
Carter Jones <carter@carterjones.info>
2015-07-15 23:38:13 -0700

    make it compile in netbeans 8 with jdk 7

--- src/com/aspect/snoop/ui/choose/process/ChooseProcessView.java
+++ src/com/aspect/snoop/ui/choose/process/ChooseProcessView.java
@@ -483,12 +483,14 @@ public class ChooseProcessView extends javax.swing.JDialog {
 		    if (MonitoredVmUtil.isAttachable(mvm)) {
 		    	attachable = true;
 		    } else {
+                        /*
 		    	if (MonitoredVmUtil.isKernelVM(mvm)) {
         	            logger.trace("Kernel VM does not support the attach mechanism");
 		    	}
 		    	else {
                             logger.trace("The VM does not support the attach mechanism for pid " + id);
             	    	}
+                        */
 		    }
 	    } catch (Throwable t) {
 		    // we do not know what this id is
